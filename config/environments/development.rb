a006eaa9cf77768687438bee5a042eb1e44ca8e6
Juan Ibiapina <juanibiapina@gmail.com>
2022-01-22 18:50:07 +0100

    chore: Add support for STDOUT logging in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
