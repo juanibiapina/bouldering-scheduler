e4d15a7c616e4e710c65a1189a989cc5b357adc5
Juan Ibiapina <juanibiapina@gmail.com>
2022-04-19 01:44:34 +0200

    fix: Fix shell.nix
    - Add chrome related controllers for testing on OSX
    - Only use firefox in linux since it's not supported on OSX
    - Add dependency on libxml2

--- shell.nix
+++ shell.nix
@@ -1,12 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    docker-compose
-    firefox
+let
+  packages = with pkgs; [
+    chromedriver
+    docker
     geckodriver
     heroku
+    libxml2
     postgresql
     ruby_3_1
-  ];
+  ]
+  ++ lib.optional stdenv.isLinux firefox;
+in pkgs.mkShell {
+  nativeBuildInputs = packages;
 }
