ab12ee37c767041806c58d23f839fa80386c4491
Juan Ibiapina <juanibiapina@gmail.com>
2022-04-28 11:44:05 +0200

    chore: Update ruby to 3.1.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source "https://rubygems.org"
 
-ruby "3.1.1"
+ruby "3.1.2"
 
 gem "rails", "~> 6.1.4", ">= 6.1.4.1"
 # Use postgresql as the database for Active Record
--- shell.nix
+++ shell.nix
@@ -1,16 +1,30 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  packages = with pkgs; [
+  pwd = builtins.getEnv "PWD";
+  ruby_version_string = builtins.readFile (pwd + "/.ruby-version");
+  ruby_version_parts = builtins.match "([0-9]*)\.([0-9]*)\..*" ruby_version_string;
+  ruby_package = builtins.concatStringsSep "_" (["ruby"] ++ ruby_version_parts);
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
     chromedriver
     docker
+    firefox-bin
     geckodriver
     heroku
     libxml2
     postgresql
-    ruby_3_1
-  ]
-  ++ lib.optional stdenv.isLinux firefox;
-in pkgs.mkShell {
-  nativeBuildInputs = packages;
+    pkgs.${ruby_package}
+  ];
+  shellHook = ''
+    # install gems locally
+    mkdir -p .local/nix-gems
+    export GEM_HOME=$PWD/.local/nix-gems
+    export GEM_PATH=$GEM_HOME
+    export PATH=$GEM_HOME/bin:$PATH
+
+    # add local bin directory to path
+    export PATH=$PWD/bin:$PATH
+  '';
 }
